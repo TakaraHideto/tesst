ba59c1a4edc795cc91ecdf6e1e3ab3a07bf3c291
TakaraHideto <t-hideto-2411@occ.co.jp>
2024-04-24 11:19:10 +0900

    i

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -2,43 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      {/* ヘッダー */}
-      <header className="bg-blue-500 text-purple-700">
-        <div className="container mx-auto flex justify-between items-center">
-          <h1 className="text-2xl font-bold">ようこそ怪しいサイトへ</h1>
-          <nav>
-            <ul className="flex space-x-4">
-              <li><a href="#" className="hover:underline">ホーム</a></li>
-              <li><a href="#" className="hover:underline">メニュー</a></li>
-              <li><a href="#" className="hover:underline">サービス</a></li>
-              <li><a href="#" className="hover:underline">ログアウト</a></li>
-            </ul>
-          </nav>
-        </div>
-      </header>
-
-      {/* メインコンテンツ */}
-      <main className="bg-white py-10">
-        <div className="container mx-auto">
-          <section className="text-center">
-            <h2 className="text-3xl font-bold text-gray-800 mb-4">ハイサイ！ ここは沖縄です！</h2>
-            <p className="text-lg text-gray-600">This is pen!</p>
-          </section>
-
-          <section className="flex justify-center mt-10">
-            <div className="rounded-full h-80 w-80 bg-red-500 flex items-center justify-center">
-              <p className="text-7xl text-amber-950 font-bold text-center">Hello Japan!</p>
-            </div>
-          </section>
-        </div>
-      </main>
-
-      {/* フッター */}
-      <footer className="bg-blue-500 text-white py-4 text-center">
-        <p>&copy; 2024 My Awesome Website. All rights reserved.</p>
-      </footer>
-    </div>
-   
+    <main className="relative h-screen flex justify-center items-end">
+      <div className="absolute left-10 top-0 mt-8 ml-8">
+        <p className="text-2xl font-bold text-gray-800">TailwindTemplates</p>
+      </div>
+    </main>
   );
 }
